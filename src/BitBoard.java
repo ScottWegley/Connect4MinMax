24407421cfdea8ea824b3fe0856a60e6e440d603
Scott Wegley <scbrwegley@protonmail.ch>
2023-09-26 14:01:08 -0700

    get a board view from a long[]

--- src/BitBoard.java
+++ src/BitBoard.java
@@ -10,4 +10,26 @@ class BitBoard {
     public static void printLong(long l) {
         System.out.println(Long.toBinaryString(l));
     }
+
+    public static String boardViewFromLong(long[] bb) {
+
+        String out = "";
+
+        for (int i = 5; i >= 0; i--) {
+            for (int j = 0; j < 7; j++) {
+                char c = '.';
+                if (((bb[0] >> (i + j * 7)) & 0x1) == 1) {
+                    c = 'X';
+                } else if (((bb[1] >> (i + j * 7)) & 0x1) == 1) {
+                    c = 'O';
+                }
+                out = out + c + " ";
+            }
+            out = out + '\n';
+        }
+
+        out = out + "-------------\n";
+        out = out + "0 1 2 3 4 5 6\n";
+        return out;
+    }
 }
