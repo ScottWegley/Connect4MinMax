5688d5bd10a80ad112076b8c7b8052f63442f262
Scott Wegley <scbrwegley@protonmail.ch>
2023-09-26 14:00:03 -0700

    empty class to store a bitboard starts with empty boards and heights at default indexes

---
+++ src/BitBoard.java
@@ -0,0 +1,10 @@
+/*
+ * This file was written as I read through https://github.com/denkspuren/BitboardC4/blob/master/BitboardDesign.md,
+ * a very helpful explanation of 
+ */
+
+class BitBoard {
+    private long[] _bb = { 0, 0 };
+    private int[] _height = { 0, 7, 14, 21, 28, 35, 42 };
+
+}
