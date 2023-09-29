deb286751b588a61136700dcbb224aeb687e817a
Scott Wegley <scbrwegley@protonmail.ch>
2023-09-29 08:23:31 -0700

    added needed member vars + constructors

--- src/BitBoard.java
+++ src/BitBoard.java
@@ -4,10 +4,46 @@
  */
 
 class BitBoard {
-    private long[] _bb = { 0, 0 };
-    private int[] _height = { 0, 7, 14, 21, 28, 35, 42 };
+    /**
+     * This is an array of two longs. The first when read in binary stores the
+     * positions of Player 0, the second of Player 1.
+     */
+    private final long[] _bb = { 0, 0 };
+
+    /**
+     * This array stores the next index for a move to be played in each column.
+     * It's initialized to the bit at the bottom of each column.
+     */
+    private final int[] _height = { 0, 7, 14, 21, 28, 35, 42 };
+
+    /**
+     * Keeps track of how many moves have been made. When it is even, it's player
+     * 1's turn, odd is Player 1.
+     */
+    private int _counter = 0;
+
+    private int X_PLAYER_NUM;
+    private int O_PLAYER_NUM;
+
+    /**
+     * This array stores every move made in this instance of Connect 4. There are
+     * only 42 positions, so we only need to store 42 moves.
+     */
+    private final int[] _moves = new int[42];
 
     BitBoard() {
+        this(0, 1);
+    }
+
+    BitBoard(int xNum, int oNum) {
+        if ((xNum == 0 || xNum == 1) && (oNum == 0 || oNum == 1) && xNum != oNum) {
+            X_PLAYER_NUM = xNum;
+            O_PLAYER_NUM = oNum;
+        } else {
+            X_PLAYER_NUM = 0;
+            O_PLAYER_NUM = 1;
+        }
+    }
     }
 
     public static void main(String[] args) {
