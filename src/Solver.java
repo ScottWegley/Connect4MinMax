9d9d5244352c8c686f47c2924e1efdbb7ab8e282
scottwegley <scottwegley@gmail.com>
2023-10-01 15:52:07 -0700

    negamax function to grade a position

--- src/Solver.java
+++ src/Solver.java
@@ -1,4 +1,26 @@
 package src;
 
 public class Solver {
+    public static int negamax(BitBoard bb) {
+        if (bb.isDraw()) {
+            return 0;
+        }
+        for (int i = 0; i < bb.listMoves().length; i++) {
+            if (bb.listMoves()[i] != -1 && bb.isWinningMove(i)) {
+                return (43 - bb.getTurnCount()) / 2;
+            }
+        }
+
+        int bestScore = -42;
+        for (int i = 0; i < bb.listMoves().length; i++) {
+            if (bb.listMoves()[i] != -1) {
+                bb.makeMove(i);
+                int score = -negamax(bb);
+                if (score > bestScore)
+                    bestScore = score;
+                bb.undoMove();
+            }
+        }
+        return bestScore;
+    }
 }
