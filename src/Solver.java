8d3295bc6e4a473dd3f850eedaf2bbd1fbf5b169
scottwegley <scottwegley@gmail.com>
2023-10-01 23:42:30 -0700

    ok alpha beta is 100% working here

--- src/Solver.java
+++ src/Solver.java
@@ -1,26 +1,39 @@
 package src;
 
 public class Solver {
-    public static int negamax(BitBoard bb) {
+
+    private static final int[] EXPLORE_ORDER = { 3, 2, 4, 1, 5, 0, 6 };
+
+    public static int negamax(BitBoard bb, int alpha, int beta) {
         if (bb.isDraw()) {
             return 0;
         }
         for (int i = 0; i < bb.listMoves().length; i++) {
-            if (bb.listMoves()[i] != -1 && bb.isWinningMove(i)) {
+            if (bb.listMoves()[EXPLORE_ORDER[i]] != -1 && bb.isWinningMove(EXPLORE_ORDER[i])) {
                 return (43 - bb.getTurnCount()) / 2;
             }
         }
 
-        int bestScore = -42;
-        for (int i = 0; i < bb.listMoves().length; i++) {
-            if (bb.listMoves()[i] != -1) {
-                bb.makeMove(i);
-                int score = -negamax(bb);
-                if (score > bestScore)
-                    bestScore = score;
-                bb.undoMove();
+        int max = (41 - bb.getTurnCount()) / 2;
+        if (beta > max) {
+            beta = max;
+            if (alpha >= beta) {
+                return beta;
             }
         }
-        return bestScore;
+
+        for (int i = 0; i < bb.listMoves().length; i++) {
+            if (bb.listMoves()[EXPLORE_ORDER[i]] != -1) {
+                BitBoard b2 = new BitBoard(bb);
+                b2.makeMove(EXPLORE_ORDER[i]);
+                int score = -negamax(b2, -beta, -alpha);
+                if (score >= beta)
+                    return score;
+                if (score > alpha) {
+                    alpha = score;
+                }
+            }
+        }
+        return alpha;
     }
 }
